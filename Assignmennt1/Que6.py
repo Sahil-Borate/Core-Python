3b06a0b5a949992397a7a6d9845fd3ba14c581e0
Sahil Borate <sahilborate50@gmail.com>
2026-02-16 22:54:06 +0530

    Delete Assignmennt1 directory
    delete message

--- Assignmennt1/Que6.py
+++
@@ -1,9 +0,0 @@
-#declare the two variable and take the input from user
-Angle_A = int(input("enter the value of angle A "))
-Angle_B = int(input("enter the value of angle B "))
-
-#calculate the third angle
-Angle_c = 180 - (Angle_A + Angle_B) 
-
-#display the result
-print("the third angle of triangLe is",Angle_c)
